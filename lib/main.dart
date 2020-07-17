dc67bd9731a789bc9e3117424533cedf993b5832
sheeeeeeeeji <groovy1015@gmail.com>
2020-07-18 00:52:24 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('uouuo'),
+        title: Text('uouuop'),
         actions: <Widget>[Icon(Icons.add), Icon(Icons.share),],
       ),
       body: Container(
